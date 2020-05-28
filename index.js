e2a85cf31e5e9023b667726a69a3e5984411a23a
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-28 16:57:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function yourAnimal() {
   const animal = 'cat';
   return animal
 }
-
+const two = 2
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  two = 2
+
 }
